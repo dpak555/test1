51826a282fa62d815d03f79768bd5c370e7c6292
dpak555 <sahudeepak154@gmail.com>
2018-08-31 16:53:03 +0530

    Update UserServiceImpl.java

--- src/com/vaannila/service/UserServiceImpl.java
+++ src/com/vaannila/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public void add(User user) {
 		//Persist the user object here. 
-		logger.debug("User added successfully");
+		logger.log("User added successfully");
 
 	}
 
