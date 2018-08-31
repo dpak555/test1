ce8e23e4f2cf64ae7af1ebf3ff776bce2306313c
dpak555 <sahudeepak154@gmail.com>
2018-08-31 19:36:20 +0530

    Update MyTest.java

--- src/com/vaannila/Test/MyTest.java
+++ src/com/vaannila/Test/MyTest.java
@@ -1,9 +1,22 @@
+package com.vaannila.Test;
+ 
 import org.junit.Test;
-import org.junit.Assert.assertTrue;
-@Test
-class TestMain {
-  @Test
-  def runSamples : Unit = {
-     assertTrue(true);
-  }
+import static org.junit.Assert.*;
+ 
+public class MyAssertTrueTest {
+ 
+    public boolean isEvenNumber(int number){
+         
+        boolean result = false;
+        if(number%2 == 0){
+            result = true;
+        }
+        return result;
+    }
+     
+    @Test
+    public void evenNumberTest(){
+        MyAssertTrueTest asft = new MyAssertTrueTest();
+        assertTrue(asft.isEvenNumber(4));
+    }
 }
