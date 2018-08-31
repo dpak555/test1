25c97f8977697e660fa372bca1ee26b41f87fc2b
dpak555 <sahudeepak154@gmail.com>
2018-08-31 16:58:01 +0530

    Update UserServiceImpl.java

--- src/com/vaannila/service/UserServiceImpl.java
+++ src/com/vaannila/service/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package com.vaannila.service;
 
 import com.vaannila.domain.User;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.*;
 
 public class UserServiceImpl implements UserService {
-
+        static Logger log = Logger.getLogger(UserServiceImpl.class.getName());
 	@Override
-	public void add(User user) {
+	public void add(User user) throws IOException,SQLException {
 		//Persist the user object here. 
-		logger.log("User added successfully");
+		log.debug("User added successfully");
 
 	}
 
